cfb9061c928f2ab1c562abb078953f619f7d5875
JosephValencia <joseph.valencia@tamu.edu>
2022-11-20 17:23:54 -0600

    updating basic working version of get categories endpoint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from cv2 import cornerEigenValsAndVecs
-from fastapi import FastAPI, File, Response, UploadFile
+from fastapi import FastAPI, File, HTTPException, Response, UploadFile
 from matplotlib.pyplot import ginput
 from pydantic import BaseModel
 from enum import Enum
@@ -9,7 +9,7 @@ import socket
 from PIL import Image
 import io
 from fastapi.middleware.cors import CORSMiddleware
-
+import uvicorn
 
 
 app = FastAPI()
@@ -27,6 +27,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 class AlcoholType(Enum):
     Beer = 1
@@ -49,14 +50,25 @@ class AlcoholType(Enum):
     Scotch = 18
     Vodka = 19
 
+
 class NewItem(BaseModel):
     name: str
     description: str
     alcoholType: AlcoholType
     b64ImgStr: str
 
+
+class NewCategory(BaseModel):
+    name: str
+    submittedBy: str
+
+    def as_dict(self):
+        return {"name": self.name, "submittedBy": self.submittedBy}
+
+
 class NewRating(BaseModel):
     rating: int
+
 
 class ItemResponse(BaseModel):
     itemId: int
@@ -67,92 +79,143 @@ class ItemResponse(BaseModel):
     rating: float
     numRatings: int
 
+
 class ItemsResponse(BaseModel):
     items: list[ItemResponse] = []
+
+
+class CategoryResponse(BaseModel):
+    name: str
+    submittedBy: str
+
+
+class CategoriesResponse(BaseModel):
+    items: list[CategoryResponse] = []
+
 
 class SuccessMessage(BaseModel):
     successMessage: str
 
-items_columns = ['name', 'description', 'alcohol_type', 'b64_img_str', 'avg_rating', 'num_ratings']
-ratings_columns = ['item_id', 'rating']
 
-items_df = pd.DataFrame()
-ratings_df = pd.DataFrame()
-    
+categories_columns = ["name", "submittedBy"]
+items_columns = [
+    "name",
+    "description",
+    "alcohol_type",
+    "b64_img_str",
+    "avg_rating",
+    "num_ratings",
+]
+ratings_columns = ["item_id", "rating"]
+
+categories_df = pd.DataFrame(columns=categories_columns)
+items_df = pd.DataFrame(columns=items_columns)
+ratings_df = pd.DataFrame(columns=ratings_columns)
+
+
 def checkitemExists(itemId):
     if itemId not in items_df.index:
-        raise FastAPI.HTTPException(status_code=409, detail=f'Item does not exist')
+        raise FastAPI.HTTPException(status_code=409, detail=f"Item does not exist")
+
 
 def checkRatingValidInputType(rating):
     if type(rating) != int:
-        raise FastAPI.HTTPException(status_code=409, detail=f'Non-numeric rating entered for item')
+        raise FastAPI.HTTPException(
+            status_code=409, detail=f"Non-numeric rating entered for item"
+        )
+
 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(0)
     try:
         # doesn't even have to be reachable
-        s.connect(('10.254.254.254', 1))
+        s.connect(("10.254.254.254", 1))
         IP = s.getsockname()[0]
     except Exception:
-        IP = '127.0.0.1'
+        IP = "127.0.0.1"
     finally:
         s.close()
     return IP
 
-def image_to_byte_array(image: Image) -> bytes:
-  # BytesIO is a fake file stored in memory
-  imgByteArr = io.BytesIO()
-  # image.save expects a file as a argument, passing a bytes io ins
-  image.save(imgByteArr, format=image.format)
-  # Turn the BytesIO object back into a bytes object
-  imgByteArr = imgByteArr.getvalue()
-  return imgByteArr
 
-@app.get('/{port}/qr-code',     
-    responses = {
-        200: {
-            "content": {"image/png": {}}
-        }
-    },
-     response_class=Response)
-async def getQrCode(port, webPath='', fillColor='black', backgroundColor='white'):
-    address = 'http://'+get_ip()+':'+port+'/'+webPath
+def image_to_byte_array(image: Image) -> bytes:
+    # BytesIO is a fake file stored in memory
+    imgByteArr = io.BytesIO()
+    # image.save expects a file as a argument, passing a bytes io ins
+    image.save(imgByteArr, format=image.format)
+    # Turn the BytesIO object back into a bytes object
+    imgByteArr = imgByteArr.getvalue()
+    return imgByteArr
+
+
+@app.get(
+    "/{port}/qr-code",
+    responses={200: {"content": {"image/png": {}}}},
+    response_class=Response,
+)
+async def getQrCode(port, webPath="", fillColor="black", backgroundColor="white"):
+    address = "http://" + get_ip() + ":" + port + "/" + webPath
     # Creating an instance of QRCode class
-    qr = qrcode.QRCode(version = 1,
-                    box_size = 10,
-                    border = 5)
-    
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+
     # Adding data to the instance 'qr'
     qr.add_data(address)
-    
-    qr.make(fit = True)
-    img = qr.make_image(fill_color = fillColor,
-                        back_color = backgroundColor)
-    
+
+    qr.make(fit=True)
+    img = qr.make_image(fill_color=fillColor, back_color=backgroundColor)
+
     return Response(content=image_to_byte_array(img), media_type="image/png")
 
 
-@app.get('/all-items', response_model=ItemsResponse)
+@app.get("/all-categories", response_model=CategoriesResponse)
+async def getAllCategories():
+    global categories_df
+    print("======================")
+    temp = CategoriesResponse(
+        items=[
+            CategoryResponse(**kwargs)
+            for kwargs in categories_df.to_dict(orient="records")
+        ]
+    )
+    print(temp)
+    return temp
+
+
+@app.get("/all-items", response_model=ItemsResponse)
 async def getAllItems():
     return items_df
 
-@app.get('/item/{item_id}', response_model=ItemResponse)
+
+@app.get("/item/{item_id}", response_model=ItemResponse)
 async def getItemById(itemId):
     checkitemExists(itemId)
     return items_df.loc[itemId]
 
-@app.post('/new-item/', response_model=SuccessMessage)
+
+@app.post("/new-item/", response_model=SuccessMessage)
 async def createNewItem(item: NewItem):
-    if item.name.lower() in items_df['name'].lower():
-        raise FastAPI.HTTPException(status_code=409, detail=f'Item already exists')
+    if item.name.lower() in items_df["name"].lower():
+        raise FastAPI.HTTPException(status_code=409, detail=f"Item already exists")
 
     items_df.append(item)
 
-    return {'New item created successfully'}
+    return {"New item created successfully"}
 
 
-@app.post('/rating/{item_id}', response_model=SuccessMessage)
+@app.post("/category/", response_model=SuccessMessage)
+async def createNewCategory(category: NewCategory):
+    global categories_df
+    if category.name.lower() in categories_df["name"].values.tolist():
+        raise HTTPException(status_code=409, detail=f"Category already exists")
+
+    newCategoryDf = pd.DataFrame.from_dict([category.as_dict()])
+    categories_df = categories_df.append(newCategoryDf)
+
+    return {"successMessage": "New category created successfully"}
+
+
+@app.post("/rating/{item_id}", response_model=SuccessMessage)
 async def createNewRating(itemId, rating: NewRating):
     # Check for invalid data entries
     checkitemExists(itemId)
@@ -162,10 +225,14 @@ async def createNewRating(itemId, rating: NewRating):
     ratings_df.append(rating)
 
     # Calculate avg
-    item_only_df=ratings_df.query('Courses == {}'.format(itemId))
-    newAvg = item_only_df['rating'].mean()
+    item_only_df = ratings_df.query("Courses == {}".format(itemId))
+    newAvg = item_only_df["rating"].mean()
 
     # Set new avg
-    items_df.at[itemId,'avg_rating']=newAvg
+    items_df.at[itemId, "avg_rating"] = newAvg
 
-    return {'Rating entered successfully!'}
+    return {"Rating entered successfully!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
